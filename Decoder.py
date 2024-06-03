a076214d327bd09859348e687248cdd70211b9a3
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-06-03 12:16:50 +0530

    Update Decoder.py
    The `Decoder` module is a component of sequence-to-sequence models, commonly used in tasks like language translation. It processes the encoded input sequence and generates the output sequence. It typically consists of multiple decoder layers, each containing self-attention and cross-attention mechanisms, followed by position-wise feed-forward networks. These layers help the decoder understand the input context and generate accurate output predictions.

--- Decoder.py
+++ Decoder.py
@@ -74,4 +74,17 @@ class Decoder(nn.Module):
         #mask : 200 x 200
         y = self.layers(x, y, mask)
         return y #30 x 200 x 512
-     
+d_model = 512
+num_heads = 8
+drop_prob = 0.1
+batch_size = 30
+max_sequence_length = 200
+ffn_hidden = 2048
+num_layers = 5
+
+x = torch.randn( (batch_size, max_sequence_length, d_model) ) 
+y = torch.randn( (batch_size, max_sequence_length, d_model) ) 
+mask = torch.full([max_sequence_length, max_sequence_length] , float('-inf'))
+mask = torch.triu(mask, diagonal=1)
+decoder = Decoder(d_model, ffn_hidden, num_heads, drop_prob, num_layers)
+out = decoder(x, y, mask)
